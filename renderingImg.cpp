83862969d3dc7f518ebe1b9694d4dac0982e90bd
Wizock <Rohaanerodasahmed@gmail.com>
2021-04-14 03:45:06 +1000

    Add project files.

---
+++ renderingImg.cpp
@@ -0,0 +1,19 @@
+///
+/// file document and reference https://lazyfoo.net/tutorials/SDL/02_getting_an_image_on_the_screen/index.php
+/// 
+
+#include <iostream>
+#include <SDL.h>
+
+using namespace std;
+
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
+
+
